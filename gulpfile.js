aad6f2cd720464141d9930a018240c7fb1acc258
Maud Leray <mleray@insa-rennes.fr>
2016-10-20 23:14:57 +0200

    added .gitignore + working basic react/gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 const gulp = require("gulp");
 const less = require("gulp-less");
+const browserSync = require("browser-sync").create();
+const react = require("gulp-react");
+const browserify = require("browserify");
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
 
-gulp.task("less", function(){
-	return gulp.src("app/less/*.less")
-		.pipe(less())
-		.pipe(gulp.dest("app/css/"))
+gulp.task("build", function () {
+    return browserify({ entries: "src/jsx/App.jsx", extensions: [".jsx"], debug: true })
+        .transform("babelify", { presets: ["es2015", "react"] })
+        .bundle()
+        .pipe(source("bundle.js"))
+        .pipe(gulp.dest("dist"));
 });
+
+gulp.task("watch", ["build"], function () {
+    gulp.watch("src/jsx/*.jsx", ["build"]);
+});
+
+gulp.task("default", ["watch"]);
---
+++ src/jsx/App.jsx
@@ -0,0 +1,8 @@
+import HelloWorld from "./HelloWorld";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <HelloWorld phrase="ES6"/>,
+    document.querySelector(".root")
+);
