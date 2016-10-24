8795948248fee4202f023f48c5fb5413ae80d0fb
Maud Leray <maud.leray035@gmail.com>
2016-10-24 17:04:08 +0200

    changed chart orientation to horizontal

--- src/js/ui/Bar.jsx
+++ src/js/ui/Bar.jsx
@@ -1,27 +1,26 @@
 import React, { PropTypes } from "react";
 
-const Bar = ({ width, height, offset, color, availableHeight }) => (
+const Bar = ({ width, height, x, color, y }) => (
 	<rect 
 		fill={color}
 	    width={width} 
 	    height={height} 
-	    x={offset} 
-	    y={availableHeight - height}
+	    x={x} 
+	    y={y}
 	/>
 );
 
 Bar.propTypes = {
     width: PropTypes.number,
     height: PropTypes.number,
-    offset: PropTypes.number,
+    x: PropTypes.number,
     color: PropTypes.string,
-    availableHeight: PropTypes.number
+    y: PropTypes.number
 };
 
 Bar.defaultProps = {
 	width: 0,
-    height: 0,
-    offset: 0
+    height: 0
 };
 
 module.exports = Bar;
--- src/js/ui/DataSeries.jsx
+++ src/js/ui/DataSeries.jsx
@@ -4,27 +4,28 @@ import Bar from "./Bar";
 class DataSeries extends Component {
   
     render() {
-
         const { data, height, width, color } = this.props;
-
-        // Set up domain and range for y
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(data)])
-            .range([0, height]);
-
         // Set up domain and range for x
-        const x = d3.scaleBand()
-            .domain([1, 2, 3])
+        const x = d3.scaleLinear()
+            .domain([0, d3.max(data)])
             .range([0, width]);
 
+        // Set up domain and range for y
+        const y = d3.scaleBand()
+            .domain([1,2,3])
+            .range([height, 0]);
+
         // Generate bars for the chart
-        const bars = data.map((point, i) => {
+        // d is the number of times the answer has been sent, i is the index
+        const bars = data.map((d, i) => {
+            console.debug("x(d)", x(d));
+            console.debug("y(i+1)", y(i+1));
             return (
                 <Bar 
-                    height={y(point)}
-                    width={0.8 * x.bandwidth()}
-                    offset={x(i+1)} // i+1 because i is in [0,1,2] but the values for x are in [1,2,3]
-                    availableHeight={height} 
+                    width={x(d)}
+                    height={0.8 * y.bandwidth()}
+                    x={0}
+                    y={y(i+1)} // i+1 because i is in [0,1,2] but the anwers'numbers are in [1,2,3] 
                     color={color} 
                     key={i} 
                 />
